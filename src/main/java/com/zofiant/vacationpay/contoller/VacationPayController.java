e747f960fa5ee09223a2ac50b7f712662a350dd8
bake <mr.bisquit200@gmail.com>
2025-04-20 23:14:14 +0300

    Deleted unused depend

--- src/main/java/com/zofiant/vacationpay/contoller/VacationPayController.java
+++ src/main/java/com/zofiant/vacationpay/contoller/VacationPayController.java
@@ -2,9 +2,7 @@ package com.zofiant.vacationpay.contoller;
 
 import com.zofiant.vacationpay.dto.PayInfoDto;
 import com.zofiant.vacationpay.service.VacationPayService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +16,6 @@ public class VacationPayController {
 
     @GetMapping(path = "/sumVacationPay")
     private double sumVacationPay( PayInfoDto payInfoDto){
-        double vacationPay = vacationPayService.calculateVacationPay(payInfoDto);
-        return vacationPay;
+        return vacationPayService.calculateVacationPay(payInfoDto);
     }
 }
